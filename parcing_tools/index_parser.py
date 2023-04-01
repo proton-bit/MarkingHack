c5bc29d7e87486327dae33d078042960885ebf16
Said Azizov <protonpetabyte@gmail.com>
2023-04-02 00:15:22 +0300

    interface update

--- parcing_tools/index_parser.py
+++ parcing_tools/index_parser.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 
 def get_data_frame() -> pd.DataFrame:
-    index_dbf = DBF("../raw_data/index.dbf")
+    index_dbf = DBF("raw_data/index.dbf")
     data_frame = pd.DataFrame(index_dbf)
     return data_frame
 
