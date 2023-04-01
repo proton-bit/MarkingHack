06f79b5c35a9473114d086bccf4976fe59a4c46e
Said Azizov <protonpetabyte@gmail.com>
2023-04-01 15:18:50 +0300

    streamlit interface

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ OUTPUT_CIRCULATION_FILENAME = f"{DATASET_PATH}/Данные о выводе то
 TRANSITION_CICULATION_FILENAME = f"{DATASET_PATH}/Данные о перемещениях товаров между участниками с 2021-11-22 по 2022-11-21.csv"
 OUTLETS_HANDBOOK_FILENAME = f"{DATASET_PATH}/Справочник торговых точек.csv"
 PARTICIPATORS_HANDBOOK_FILENAME = f"{DATASET_PATH}/Справочник участников оборота товаров.csv"
+PRODUCT_HANDBOOK_FILENAME = f"{DATASET_PATH}/Справочник участников оборота товаров.csv"
 
 INPUT_CIRCULATION_PARQUET_FILENAME = f"{DATASET_PATH}/input.parquet"
 OUTPUT_CIRCULATION_PARQUET_FILENAME = f"{DATASET_PATH}/output.parquet"
