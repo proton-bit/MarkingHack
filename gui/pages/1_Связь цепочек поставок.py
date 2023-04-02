de75479a902920a5d895e503f1a7e8cfd86e8234
Said Azizov <protonpetabyte@gmail.com>
2023-04-02 07:58:30 +0300

    some fixes

--- gui/pages/1_Связь цепочек поставок.py
+++ gui/pages/1_Связь цепочек поставок.py
@@ -3,15 +3,10 @@ import pandas as pd
 import pydeck as pdk
 from urllib.error import URLError
 
-st.set_page_config(page_title="Mapping Demo", page_icon="🌍")
+st.set_page_config(page_title="Supply Chain Map", page_icon="🌍")
 
-st.markdown("# Mapping Demo")
-st.sidebar.header("Mapping Demo")
-st.write(
-    """This demo shows how to use
-[`st.pydeck_chart`](https://docs.streamlit.io/library/api-reference/charts/st.pydeck_chart)
-to display geospatial data."""
-)
+st.markdown("# Supply Chain Map")
+st.sidebar.header("Supply Chain Map")
 
 
 @st.cache_data
