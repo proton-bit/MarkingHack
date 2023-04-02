03ef953fd0107f9dbc164712c97edbc73ec1928b
Said Azizov <protonpetabyte@gmail.com>
2023-04-02 15:49:28 +0300

    final commit

--- to_parquets.py
+++ to_parquets.py
@@ -2,14 +2,12 @@ import pandas as pd
 import numpy as np
 from config import *
 
-input_data = pd.read_parquet(INPUT_CIRCULATION_FILENAME).iloc[::5]
-transition_data = pd.read_parquet(TRANSITION_CICULATION_FILENAME).iloc[::100]
+input_data = pd.read_parquet("dataset/input.parquet").iloc[::5]
+transition_data = pd.read_parquet("dataset/transition.parquet").iloc[::100]
 output_data = pd.read_parquet(
-    OUTPUT_CIRCULATION_FILENAME,
-    dtype={'price': np.uint32, 'cnt': np.uint32},
-    parse_dates=['dt']
+    "dataset/output.parquet"
 ).iloc[::200]
 
-input_data.to_parquet(INPUT_CIRCULATION_PARQUET_FILENAME)
-transition_data.to_parquet(TRANSITION_CIRCULATION_PARQUET_FILENAME)
-output_data.to_parquet(OUTPUT_CIRCULATION_PARQUET_FILENAME)
+input_data.to_parquet("dataset/input_small.parquet")
+transition_data.to_parquet("dataset/transition.parquet")
+output_data.to_parquet("dataset/output.parquet")
